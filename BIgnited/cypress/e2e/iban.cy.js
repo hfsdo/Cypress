4e896e3eea1b4455970fdd602261c6ee96f3cd3d
hfsdo <thijsdl@gmail.com>
2024-03-02 15:26:59 +0100

    Added IBAN calculation

--- BIgnited/cypress/e2e/iban.cy.js
+++ BIgnited/cypress/e2e/iban.cy.js
@@ -20,9 +20,29 @@ describe('IBAN Testing', () => {
 
     cy.get('#iban-text').invoke('text').should(($p) => {
       expect(($p.match(/\n/g)||[]).length).equal(amount-1)
-      
     })
-
   })
+
+  it('can generate multiple valid IBAN\'s', () => {
+    var amount = 5
+    cy.get('[id$=iban-1]').type(amount)
+    cy.get('[id$=iban-generate-button]').click()
+    cy.get('#iban-text').should('be.visible').invoke('text').should('not.be.empty')
+
+    cy.get('#iban-text').invoke('text').should(($p) => {
+      expect(($p.match(/\n/g)||[]).length).equal(amount-1)
+      var ibanArr = $p.split('\n')
+      ibanArr.forEach(checkValidIban)
+    })
+  })
+
+  function checkValidIban(item, index, arr) {
+    var checkval = item.replaceAll(' ', '')
+    checkval = checkval.substring(4)+checkval.substring(0,4)
+    checkval = checkval.replaceAll(/[A-Z]/g, m => m.charCodeAt() - 64+9)
+    checkval = BigInt(checkval) % BigInt(97)
+    expect(parseInt(checkval)).equals(1)
+  }
+
 })
 
